540b8f4b004f9e48ba4d1d697944ccc5d4a3521a
hezron-ngoma-hezzykialo <hezzykialo.official.ke@gmail.com>
2022-03-13 19:48:13 +0300

    at mains.css the toggle have display none ,Js file the code of toggle

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-$("document").ready(function(){
-    $(".design").click(function(){
-        $(".first-toggle").toggle();
-    });
-})
+//first-toggle
+$(document).ready(function () {
+  $(".design").click(function () {
+    $(".first-toggle").toggle();
+  });
+});
+
+//second-toggle
+$(document).ready(function () {
+  $(".development").click(function () {
+    $(".second-toggle").toggle();
+  });
+});
+
+//third toggle
+$(document).ready(function () {
+  $(".products").click(function () {
+    $(".third-toggle").toggle();
+  });
+});
