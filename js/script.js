d8b33bdbf4a08c874ed31f4eb29043fef3e64c2c
hezron-ngoma-hezzykialo <hezzykialo.official.ke@gmail.com>
2022-03-14 16:04:38 +0300

    What we can do section: toggle is working and portfolio section : hover is done

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 //init contact-form
-const username = document.getElementById("nameId").value;
+const name = document.getElementById("nameId").value;
 const email = document.getElementById("emailId").value;
 const userMessage = document.getElementById("textArea").value;
 
 //first-toggle
 $(document).ready(function () {
   $(".design").click(function () {
-    $(".first-toggle").toggle(slow);
+    $(".first-toggle").toggle();
     $(".card-header1").toggle();
   });
 });
@@ -14,7 +14,7 @@ $(document).ready(function () {
 //second-toggle
 $(document).ready(function () {
   $(".development").click(function () {
-    $(".second-toggle").toggle(slow);
+    $(".second-toggle").toggle();
     $(".card-header2").toggle();
   });
 });
@@ -22,14 +22,10 @@ $(document).ready(function () {
 //third toggle
 $(document).ready(function () {
   $(".products").click(function () {
-    $(".third-toggle").toggle(slow);
+    $(".third-toggle").toggle();
     $(".card-header3").toggle();
   });
 });
-
-// form.addEventListener("submit", validate =>{
-
-// })
 
 //function to output msg to user
 $(document).ready(function () {
@@ -42,10 +38,11 @@ $(document).ready(function () {
 });
 
 //portfolio hover
-$(".portfolio-card").hover(function () {
-  $('.description',this).show(600)
-},
-function(){
-  $('.description').hide()
-}
+$(".portfolio-card").hover(
+  function () {
+    $(".description", this).show(600);
+  },
+  function () {
+    $(".description").hide();
+  }
 );
